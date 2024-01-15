2d9abeaca5ba080f0902460e02b683ffb65fea82
THarris06 <144939694+THarris06@users.noreply.github.com>
2024-01-15 09:28:22 -0330

    class 2 @ Period 1

---
+++ First Programs/Classes_Constructor.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, name = "", price=0.0, discount_percent=0):
+        self.name = name
+        self.price = price
+        self.discount_percent = discount_percent
+
+    def getDiscountAmount(self):
+        return self.price * (self.discount_percent / 100)
+
+    def getDiscountPrice(self):
+        return self.price - self.getDiscountAmount()
