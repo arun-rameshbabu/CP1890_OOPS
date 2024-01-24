f311137f7ac687edc65d8692085072846cf00915
Ethan <Ethandrover@hotmail.com>
2024-01-24 09:41:08 -0330

    January 15th

--- Class Exercises/First_Class.py
+++ Class Exercises/First_Class.py
@@ -12,6 +12,22 @@ class Product:
     def get_discount_price(self) -> float:
         return self.__price * self.get_discount_amount()
 
+    def __post_init__(self):
+        self.price = self.__price
+
     @property
     def get_value(self) -> float:
         return self.__price
+
+    @get_value.setter
+    def price(self, price):
+        if price < 0:
+            raise ValueError("Price cannot be less than 0")
+        else:
+            self.__price = price
+
+    def getDiscountAmount(self):
+        return self.price * (self.get_discount_precent / 100)
+
+    def getDiscountPercent(self):
+        return self.price - self.getDiscountAmount()
