d2e09b4bf09b7005be2564ce62ac1eb6a151f6b6
MichaelAgonsi <144907804+MichaelAgonsi@users.noreply.github.com>
2024-02-05 10:16:13 -0330

    Michael's Time date exercise

--- Class Exercises/Datetime exercise.py
+++ Class Exercises/Datetime exercise.py
@@ -1,42 +1,54 @@
 from datetime import datetime, date, timedelta
-class Invoice:
-    def __init__(self,invoice_date):
-        self.date=invoice_date
+from dataclasses import dataclass
 
+
+@dataclass
+class Invoice:
+    date: str
 
     def getDate(self):
-        date_object = datetime.strptime(self.date,'%m/%d/%y')
+        date_object = datetime.strptime(self.date, '%m/%d/%y')
         return date_object
 
     @property
     def getDueDate(self):
-        return self.getDate() + timedelta(days=30)
+        dueDays = self.getDate() + timedelta(days=30)
+        return dueDays
+
 
 def Datetoday():
-    today = date.today()
+    today = datetime.now()
     return today
+
 
 def Overdue(Item):
     days = Datetoday() - Item.getDueDate
-    if days < 0:
+    if days.days < 0:
         print('This invoice is not yet due')
-    elif days == 0:
+    elif days.days == 0:
         print('This invoice is due today')
-    elif days > 0 :
-        print(f'This invoice is {days} day(s) overdue.')
+    elif days.days > 0:
+        print(f'This invoice is {days.days} day(s) overdue.')
+
 
 def title():
     print('The Invoice Due Date Program')
+
+
 def main():
     title()
-    invoiceDate = input('Enter the Invoice date (MM/DD/YY): ')
-    Item = Invoice(invoiceDate)
-    print()
-    print(f"{'Invoice Date':20}: {Item.getDate():%B %d, %Y}")
-    print(f"{'Due Date':20}: {Item.getDueDate:%B %d, %Y }")
-    print(f"{'Current Date':20}: {Datetoday():%B %d, %Y }")
-    print()
-    Overdue(Item)
+    cont = 'y'
+    while cont.lower() == 'y':
+        invoiceDate = input('Enter the Invoice date (MM/DD/YY): ')
+        Item = Invoice(invoiceDate)
+        print()
+        print(f"{'Invoice Date':20}: {Item.getDate():%B %d, %Y}")
+        print(f"{'Due Date':20}: {Item.getDueDate:%B %d, %Y }")
+        print(f"{'Current Date':20}: {Datetoday():%B %d, %Y }")
+        print()
+        Overdue(Item)
+        print()
+        cont = input('Continue? (y/n): ')
 
 
 if __name__ == '__main__':
