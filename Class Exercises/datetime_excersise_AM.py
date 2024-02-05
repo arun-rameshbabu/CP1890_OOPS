f7c11866f06608572f83e4159b28e786f7a5348b
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-02-05 09:47:27 -0330

    dataclass and decorator

---
+++ Class Exercises/datetime_excersise_AM.py
@@ -0,0 +1,25 @@
+from datetime import datetime, date, time, timedelta
+
+print("The Invoice Due Date program\n")
+
+while True:
+    invoice = input("Enter the invoice date (MM/DD/YYYY): ")  # 1/14/2024
+    invoice_date = datetime.strptime(invoice, "%m/%d/%Y")
+    due = "2/13/2024"
+    due_date = datetime.strptime(due, "%m/%d/%Y")
+    days = (due_date - datetime.now())
+    print()
+
+    print(f"Invoice Date: {invoice_date:%B %d, %Y}")
+    print(f"Due Date: {due_date:%B %d, %Y}")
+    print(f"Current Date: {date.today():%B %d, %Y}")
+
+    print(f"\nThis invoice is {timedelta(days.days)}(s) overdue.")
+
+    cont = input("\nContinue? (y/n): ").lower()
+    if cont == 'n':
+        break
+    elif cont != 'y':
+        print("Invalid entry, Ending Program.")
+        break
+print('Goodbye!')
