8a88e737ef11237426730d42eaa4736a599104bd
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-02-07 08:53:39 -0330

    Update datetime_excersise_AM.py

--- Class Exercises/datetime_excersise_AM.py
+++ Class Exercises/datetime_excersise_AM.py
@@ -5,7 +5,7 @@ print("The Invoice Due Date program\n")
 while True:
     invoice = input("Enter the invoice date (MM/DD/YYYY): ")  # 1/14/2024
     invoice_date = datetime.strptime(invoice, "%m/%d/%Y")
-    due = "2/13/2024"
+    due = "2/03/2024"
     due_date = datetime.strptime(due, "%m/%d/%Y")
     days = (datetime.now() - due_date)
     print()
@@ -14,7 +14,7 @@ while True:
     print(f"Due Date: {due_date:%B %d, %Y}")
     print(f"Current Date: {date.today():%B %d, %Y}")
 
-    print(f"\nThis invoice is {timedelta(days.days)}(s) overdue.")
+    print(f"\nThis invoice is {days.days}day(s) overdue.")
 
     cont = input("\nContinue? (y/n): ").lower()
     if cont == 'n':
