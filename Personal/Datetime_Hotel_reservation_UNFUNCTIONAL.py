5ab30e9663245fff910d42b4f1a23451ee08793a
BStrang213 <144375601+BStrang213@users.noreply.github.com>
2024-02-09 09:44:35 -0330

    Updated Hotel Program

--- Personal/Datetime_Hotel_reservation_UNFUNCTIONAL.py
+++ Personal/Datetime_Hotel_reservation_UNFUNCTIONAL.py
@@ -1,20 +1,47 @@
-import datetime
+import locale
+from datetime import datetime
 
 
 def get_arrival_date():
-    arrival = input("Enter Arrival Date: (YYYY/MM/DD): ")
-    arrival_date = datetime.datetime.strptime(arrival, "%B %d, %Y")
+    arrival = input("Enter Arrival Date: (YYYY-MM-DD): ")
+    arrival_date = datetime.strptime(arrival, "%Y-%m-%d")
     return arrival_date
 
 
 def get_departure_date():
-    departure = input("Enter Departure Date: (YYYY/MM/DD): ")
-    departure_date = datetime.datetime.strptime(departure, "%B %d, %Y")
+    departure = input("Enter Departure Date: (YYYY-MM-DD): ")
+    departure_date = datetime.strptime(departure, "%Y-%m-%d")
     return departure_date
+
+
+def get_nightly_rate(arrival_date):
+    month = arrival_date, "%m"
+    if month == 8:
+        nightly_rate = 105
+    else:
+        nightly_rate = 85
+    return nightly_rate
 
 
 def main():
     print("The Hotel Reservation Program")
+    print()
+    arrival = get_arrival_date()
+    departure = get_departure_date()
+    print()
+    night = get_nightly_rate(arrival)
+    days_stayed = (departure - arrival)
+    days_in = days_stayed.days
+    cost = days_in * night
+    locale.setlocale(locale.LC_ALL, "en-CA")
+    print(f"Arrival Date: {arrival:%B %d, %Y}")
+    print(f"Departure Date: {departure:%B %d, %Y}")
+    print(f"Nightly rate: {locale.currency(night)}")
+    print(f"Total Nights: {days_in}")
+    print(f"Total Price: {locale.currency(cost)}")
+
+
+print("Bye!")
 
 
 if __name__ == "__main__":
