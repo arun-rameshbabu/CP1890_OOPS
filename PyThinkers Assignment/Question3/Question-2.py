610bdfb57b50172e7fd0ce828727ea1bac3d1671
ZCZDinn <christian.dinn12@ed.cna.nl.ca>
2024-02-12 13:24:32 -0330

    Question 2 branch fix

--- PyThinkers Assignment/Question3/Question-2.py
+++ PyThinkers Assignment/Question3/Question-2.py
@@ -30,3 +30,36 @@ def getCustomers(object_list):
         reader = csv.reader(file)
         for row in reader:
             object_list.append(Customer(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+
+def title():
+    print("Customer Viewer")
+
+def main():
+    object_list = []
+    getCustomers(object_list)
+    title()
+    loop = 'y'
+    while loop.lower() == 'y':
+        cust_id = input('\nEnter customer ID: ')
+        access = False
+        for obj in object_list:
+            if cust_id == obj.id:
+                print()
+                print(obj.fullName)
+                print(obj.fullAddress)
+                loop = input('\nContinue? (y/n): ')
+                while loop.lower() != 'y' and loop.lower() != 'n':
+                    print('Please enter "y" or "n".')
+                    loop = input('\nContinue? (y/n): ')
+                access = True
+        if access == False:
+            print("\nNo customer with that ID")
+            loop = input('\nContinue? (y/n): ')
+            while loop.lower() != 'y' and loop.lower() != 'n':
+                print('Please enter "y" or "n".')
+                loop = input('\nContinue? (y/n): ')
+    print("\nBye!")
+
+
+if __name__ == '__main__':
+    main()
