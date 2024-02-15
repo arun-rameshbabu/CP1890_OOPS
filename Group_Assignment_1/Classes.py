1701e0c6a4305774fcf2811e362dad1b29531b54
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-02-14 22:26:00 -0330

    Question_2 Improvements
    Does what's necessary,  just need to adjust to use classes instead of functions.

--- Group_Assignment_1/Classes.py
+++ Group_Assignment_1/Classes.py
@@ -10,47 +10,9 @@ import random
 
 @dataclass
 class Card:
-    """
-    Playing Card with Rank and Suit
-    """
-    rank:str = ""
-    suit:str = ""
-    
-    def cardName(self):
-        """
-        Returns
-        -------
-        str
-            Full name of card ex:
-                'Ace of Hearts'.
-        """
-        return f"{self.rank} of {self.suit}"
+    pass
 
 
 class Deck:
-    """
-    Deck of Playing Cards
-    """
-    def __init__(self):
-        self.deckCards = []
-    
-    def createDeck(self):
-        """
-        Generates deck.
-        """
-        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-        
-        for suit in suits:
-            for rank in ranks:
-                card = Card(rank, suit)
-                self.deckCards.append(card)
-    
-    @property
-    def shuffleDeck(self):
-        random.shuffle(self.deckCards)
-        
-    @property
-    def cardNum(self):
-        return f"{len(self.deckCards)}"
+    pass
     
--- Group_Assignment_1/Question_2.py
+++ Group_Assignment_1/Question_2.py
@@ -4,16 +4,59 @@ Assignment 1
 Question 2
 """
 
-from Classes import Card, Deck
+import random
 
-card_deck = Deck.createDeck(Card)
+
+Deck = []
+
 
 def main():
-    print(f"Card Dealer\n\nI have shuffled a deck of {card_deck.cardNum()} cards.")
+    cards(Deck)
+    print(f"Card Dealer\n\nI have shuffled a deck of {len(Deck)} cards.")
     
-    choice = int(input("\nHow many cards would you like?: "))
+    card_num = get_cards(Deck)
     
-    print("Here are your cards:")
+    print("\nHere are your cards:")
+    
+    random.shuffle(Deck)
+    
+    print_cards(card_num, Deck)
+    
+    print(f"\nThere are {len(Deck)} cards left in the deck.\n\nGood Luck!")
+    
+
+def cards(Deck):
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    
+    for suit in suits:
+        for rank in ranks:
+            card = [rank, suit]
+            Deck.append(card)
+
+
+def get_cards(Deck):
+    while True:
+        try:
+            while True:
+                choice = int(input("\nHow many cards would you like?: "))
+                if choice < 1 or choice > len(Deck):
+                    print(f"Must take atleast 1 card, and no more than the deck total ({len(Deck)}).")
+                else:
+                    break
+            return choice
+        except ValueError:
+            print("Invalid entry, try again.")
+
+
+def print_cards(num, Deck):
+    hand = []
+    
+    while num > 0:
+        hand.append(Deck.pop(0))
+        print(f"{hand[-1][0]} of {hand[-1][1]}")
+        num -= 1
+
 
 if __name__ == "__main__":
     main()
