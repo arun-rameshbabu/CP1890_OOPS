9f8732d8ea177bbe1001011427418b03ca9010d3
mcagonsi <mcagonsi.cna@gmail.com>
2024-02-18 19:55:29 -0330

    Question 2

--- PyThinkers Assignment/Question 2/Deck_Class.py
+++ PyThinkers Assignment/Question 2/Deck_Class.py
@@ -1,14 +1,19 @@
 import Card_Class
+from dataclasses import dataclass, field
 
-ranks = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
-suits = ['Clubs','Diamonds','Hearts','Spades']
-
-@dataclass
 class Deck:
-    cards:list = []
+    def __init__(self, cards = []):
+        self.cards = cards
 
-    def getDeck(self) -> list:
-        for rank in ranks:
-            for suit in suits:
-                card = Card_Class.Card(rank,suit)
+    def getDeck(self) :
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+        suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+        for suit in suits:
+            for rank in ranks:
+                card = Card_Class.Card(str(rank),suit)
                 self.cards.append(card.getCard())
+
+
+deck = Deck()
+deck.getDeck()
+print(deck.cards)
