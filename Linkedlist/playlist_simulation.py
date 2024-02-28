b0816dfd93566ddff0e642f2be20a16e9e336d40
MichaelAgonsi <144907804+MichaelAgonsi@users.noreply.github.com>
2024-02-28 08:52:18 -0330

    playlist sim

--- Linkedlist/playlist_simulation.py
+++ Linkedlist/playlist_simulation.py
@@ -11,26 +11,6 @@ class Song:
         return f'Song: {self.title}'
 
 
-@dataclass
-class Playlist:
-
-    currentSong=None
-    songList = []
-    def __repr__(self):
-        return f'Current song: {self.songList[0].data} Next song: {self.songList[1].data}'
-
-    def loadPlayList(self):
-        current_song = self.currentSong
-        while current_song is not None:
-            next_song = Song(playlist.pop())
-            self.songList.append(next_song)
-            current_song = current_song.nextSong
-        self.songList.append('Loading playlist')
-    def nextSong(self):
-        if len(self.songList) == 0:
-            print('No songs')
-        else:
-            currentSong.next =.pop(0)
 
 
 
