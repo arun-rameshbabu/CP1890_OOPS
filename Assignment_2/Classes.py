42baf25a5addc56a22c8e5623fa4d47ca13cef2d
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-03-07 15:40:07 -0330

    Started question 2

--- Assignment_2/Classes.py
+++ Assignment_2/Classes.py
@@ -5,6 +5,7 @@ Classes
 """
 
 from dataclasses import dataclass
+from random import randint
 
 # Question 1:
 @dataclass
@@ -26,3 +27,44 @@ class Customer(Person):
 @dataclass
 class Employee(Person):
     ssn: str
+
+# Question 2:
+def rand_int():
+    """
+    Generates a random integer from 1 to 100
+    
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    return randint(1, 100)
+
+class RandomIntList(list):
+    def __init__(self, num=0):
+        num = int(num)
+        while num > 0:
+            self.append(rand_int())
+            num -= 1
+
+    @property
+    def count(self):
+        return len(self)
+
+    @property
+    def total(self):
+        x = 0
+        for i in self:
+            x += i
+        return x
+
+    @property
+    def average(self):
+        return float(self.total / self.count)
+    
+    def __str__(self):
+        string = ''
+        for i in self:
+            string += f'{i} '
+        return string
---
+++ Assignment_2/Question_2.py
@@ -0,0 +1,40 @@
+"""
+Assignment 2
+
+Question 2
+"""
+
+from Classes import RandomIntList
+
+def main():
+    print("Random Integer List")
+        
+    num = input("\nHow many random integers should the list contain?: ")
+    
+    while True:
+        int_list = RandomIntList(num)
+    
+        print("\nRandom Integers\n================")
+        print(f"{'Integers:':12}{int_list}")
+        print(f"{'Count:':12}{int_list.count}")
+        print(f"{'Total:':12}{int_list.total}")
+        print(f"{'Average:':12}{int_list.average:.3f}")
+    
+        cont = contin()
+        if cont == 'n':
+            break
+        elif cont == 'y':
+            continue
+
+def contin():
+    while True:
+        cont = input("\nContinue? (y/n): ").lower()
+        if cont == 'n' or cont == 'y':
+            break
+        else:
+            print("Invalid Input\n")
+    return cont
+    
+
+if __name__ == "__main__":
+    main()
