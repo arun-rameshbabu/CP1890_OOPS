c2c2a1325edccdfa99d439060129c22267303a74
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-03-11 10:38:02 -0230

    DB

--- MyWork/Post_test_1/Databases/DB_Class_Exercise.py
+++ MyWork/Post_test_1/Databases/DB_Class_Exercise.py
@@ -4,11 +4,24 @@ import sqlite3
 conn = sqlite3.connect('class_example.sqlite')
 
 c = conn.cursor()
-query = "CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, `name` TEXT, year INTEGER, minutes INTEGER);"
+query = "CREATE TABLE IF NOT EXISTS movies (id INTEGER AUTO INCREMENT PRIMARY KEY, `name` TEXT, year INTEGER, minutes INTEGER, category TEXT);"
 c.execute(query)
-print("Table created successfully")
 
-c.execute("")
+# len = 32
+c.execute("""insert into movies (name, year, minutes, category)
+values ('Spirit: Stallion of the Cimarron', 2002, 83, 'Animation'),
+('Spirited Away', 2001, 125, 'Animation'),
+('Aladdin', 1992, 90, 'Animation'),
+('Ice Age', 2002, 81, 'Animation'),
+('Toy Story', 1995, 81, 'Animation'),
+('Titanic', 1998, 93, 'History'),
+('Transformers', 2005, 100, 'Comedy'),
+('Spooky Movie', 1995, 81, 'Comedy'),
+('Spookier Movie', 1999, 95, 'Comedy'),
+('Spookiest Movie', 2003, 94, 'Comedy'),
+('WWII: A History', 2006, 109, 'History'),
+('Movie Title', 2007, 100, 'Comedy'),
+('Why 13', 2009, 86, 'Comedy');""")
 conn.commit()
 conn.close()
 
@@ -33,8 +46,20 @@ def main():
     menu()
     categories()
 
-    com = input("Command: ")
-
+    while True:
+        com = input("Command: ")
+        if com == "cat":
+            sort_cat()
+        elif com == "year":
+            sort_year()
+        elif com == "add":
+            add_movie()
+        elif com == "del":
+            del_movie()
+        elif com == "exit":
+            break
+        else:
+            print("\nInvalid input, try again")
 
 if __name__ == "__main__":
     main()
