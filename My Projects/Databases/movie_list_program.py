6452bde98508c2930cae31d7d4004b9bd0e2df48
THarris06 <144939694+THarris06@users.noreply.github.com>
2024-03-22 10:01:33 -0230

    mar 22

--- My Projects/Databases/movie_list_program.py
+++ My Projects/Databases/movie_list_program.py
@@ -1,144 +1,125 @@
 import sqlite3
 from movie_list_classes import Category, Movie
 
-# conn = sqlite3.connect('movies.sqlite')
-# c = conn.cursor()
-# create_query = ("create table if not exists movies ("
-#          "id integer primary key autoincrement, "
-#          "Name text not null, "
-#          "Year int not null, "
-#          "Mins int not null, "
-#          "Category text not null)")
-# c.execute(create_query)
-#
-# categories = ('Animation', 'Comedy', 'History')
-conn = None
+DB_FILE = 'movies.sqlite'
 
 
-def connect_to_db(conn):
-    db_file = "movies.sqlite"
-    conn = sqlite3.connect(db_file)
+def connect():
+    conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
+    return conn
 
 
-def close_db(conn):
-    if conn:
-        conn.close()
+def close(conn):
+    conn.close()
+    print('Database Closed')
 
 
-def make_category(row):
-    return Category(row['categoryID'], row['categoryName'])
+def create_table(conn):
+    cur = conn.cursor()
+    query = ("CREATE TABLE IF NOT EXISTS categories ( "
+             "id INTEGER PRIMARY KEY, "
+             "name TEXT)")
+    cur.execute(query)
+    conn.commit()
+    query = ("CREATE TABLE IF NOT EXISTS movies ( "
+             "name TEXT PRIMARY KEY, "
+             "year INTERGER, "
+             "minutes INTEGER, "
+             "category INTEGER)")
+    cur.execute(query)
+    conn.commit()
+    print('Tables Created')
 
 
-def make_movie(row):
-    return Movie(row['movieID'], row['name'], row['year'], row['minutes'], make_category(row))
+def create_category_entries(conn):
+    cur = conn.cursor()
+    query = "INSERT INTO categories VALUES (?, ?)"
+    category_collection = ((1, 'Animation'), (2, 'Comedy'), (3, 'History'))
+    for i in range(len(category_collection)):
+        cur.execute(query, category_collection[i])
+    conn.commit()
+    print('Category entries inserted')
+
+
+def create_movie_entries(conn):
+    cur = conn.cursor()
+    query = "INSERT INTO movies VALUES (?, ?, ?, ?)"
+    movie_collection = (('Spiritted Away', 2005, 125, get_category_id(conn, 'Animation')),
+                        ('Aladdin', 1992, 90, get_category_id(conn, 'Animation')))
+    for i in range(len(movie_collection)):
+        cur.execute(query, movie_collection[i])
+    conn.commit()
+    print('Movie entries inserted')
 
 
 def get_categories(conn):
-    query = '''SELECT categoryID, categoryName FROM Category'''
-    cursor = conn.cursor()
-    cursor.excute(query)
-    results = cursor.fetchall()
-
-    categories = []
-    for row in results:
-        categories.append(make_category(row))
-    return categories
+    cur = conn.cursor()
+    query = "SELECT * FROM categories"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
 
 
-def get_category(conn, category_id):
-    query = '''SELECT categoryID, name as categoryName FROM Category WHERE categoryID = ?'''
-    cursor = conn.cursor()
-    cursor.excute(query, (category_id,))
-    row = cursor.fetchone()
-    if row:
-        return make_category(row)
+def get_category_id(conn, category_name):
+    cur = conn.cursor()
+    query = f"SELECT id FROM categories WHERE name = '{category_name}'"
+    cur.execute(query)
+    result = cur.fetchone()
+    if result:
+        return result[0]
     else:
         return None
 
 
-def make_movie_list(list_movies):
-    movies = []
-    for row in list_movies:
-        movies.append(make_movie(row))
-    return movies
+def get_category_name(conn, category_id):
+    cur = conn.cursor()
+    query = f"SELECT name FROM categories WHERE id = '{category_id}'"
+    cur.execute(query)
+    result = cur.fetchone()
+    if result:
+        return result[0]
+    else:
+        return None
 
 
-def get_movies_by_category(conn, category_id):
-    query = '''SELECT movieID, Movie.name, year, minutes, Movie.categoryID, Category.name as categoryName 
-    FROM Movie JOIN Category ON Movie.categoryID = Category.categoryID WHERE Movie.categoryID = ?'''
-    cursor = conn.cursor()
-    cursor.excute(query, (category_id,))
-    results = cursor.fetchall()
-
-    return make_movie_list(results)
+def make_category_object(conn, id):
+    category_name = get_category_name(conn, id)
+    return Category(id, category_name)
 
 
-def add_movie(conn, movie):
-    query = '''INSERT INTO Movie (categoryId, name, year, minutes) VALUES (?, ?, ?, ?)'''
-    cursor = conn.cursor()
-    cursor.excute(query, (movie.category.id, movie.name, movie.year, movie.minutes))
+def make_movie_object(conn, row):
+    return Movie(row['rowid'], row['name'], row['year'], row['minutes'], make_category_object(conn, row['category']))
+
+
+def get_movie_by_category(conn, category_id):
+    cur = conn.cursor()
+    query = f"SELECT rowid, * FROM movies WHERE category = '{category_id}'"
+    cur.execute(query)
+    result = cur.fetchall()
+    movie_list = []
+    for row in result:
+        movie_list.append(make_movie_object(conn, row))
+    return movie_list
+
+
+def add_movie(conn, movie_obj):
+    cur = conn.cursor()
+    query = f"INSERT INTO movies VALUES (?, ?, ?, ?)"
+    cur.execute(query, (movie_obj.name, movie_obj.year, movie_obj.minutes, movie_obj.category))
     conn.commit()
 
 
-def ui():
-    print("The Movie List Program\n")
-
-    print("COMMAND MENU")
-    print("cat - View movies by category")
-    print("year - View movies by year")
-    print("add - Add a movie")
-    print("del - Delete a movie")
-    print("exit - Exit program")
-
-
-def display_categories():
-    print("CATEGORIES")
-
-
-def cat_command():
-    query = ("select * from movies "
-             "order by category")
-    c.execute(query)
-
-
-def year_command():
-    query = ("select * from movies "
-             "order by year desc")
-    c.execute(query)
-
-
-# def add_command():
-#     name = input("Name: ")
-#     year = input("Year: ")
-#     category = input("Category: ")
-#
-#     query = ("insert into movies values ("
-#              "default, "
-#              "n, "
-#              "year, "
-#              "Category)")
-#
-#     c.excute(query)
-
-
-def main():
-    ui()
-
-    while True:
-        command = input("\nCommand: ")
-        if command == 'cat':
-            cat_command()
-        elif command == 'year':
-            year_command()
-        elif command == 'add':
-            add_command()
-        elif command == 'del':
-            del_command()
-        elif command == 'exit':
-            print('\nBye!')
-            break
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    conn = connect()
+    create_table(conn)
+    # create_category_entries(conn)
+    # for category in get_categories(conn):
+    #     print(category['id'], category['name'])
+    # print(get_category_id(conn, 'Comedy'))
+    # print(get_category_name(conn, 2))
+    # print(make_category_object(conn, 1))
+    # create_movie_entries(conn)
+    # print(get_movie_by_category(conn, 1))
+    some_movie = Movie(100, 'Robocop', 1994, 95, make_category_object(conn, 3))
+    close(conn)
